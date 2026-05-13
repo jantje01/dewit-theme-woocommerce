d2eac1fbf07a1847bfa09cf193a82eddc052e966
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-13 16:15:24 +0200

    Fix Elementor shop viewport layout

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.1.2' );
+define( 'DEWIT_THEME_VERSION', '0.1.3' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
