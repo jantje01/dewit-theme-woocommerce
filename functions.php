8e86f0fb006f24b809a25a43a1e3c212763bc66c
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-13 16:21:23 +0200

    Compact Elementor product list cards

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.1.4' );
+define( 'DEWIT_THEME_VERSION', '0.1.5' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
