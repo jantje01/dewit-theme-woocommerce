a651ce6236dc6da8e55ca4883ef0978a8c3d9749
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-18 15:18:14 +0200

    Use system font stack

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.1.7' );
+define( 'DEWIT_THEME_VERSION', '0.1.8' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
