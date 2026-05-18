1162a761adc26f96a837c613e2d8d073e63d1198
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-18 15:24:01 +0200

    Fix sidebar category typography

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.1.8' );
+define( 'DEWIT_THEME_VERSION', '0.1.9' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
