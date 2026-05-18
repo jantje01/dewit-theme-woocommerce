545867b74289b8f784309479cac9ffca4e3e3ede
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-18 14:45:54 +0200

    Match Elementor product card export

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.1.5' );
+define( 'DEWIT_THEME_VERSION', '0.1.6' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
