2464ccfc9a878b5b5f9b22567697bdfca268d95f
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-19 14:34:23 +0200

    Hide shop sidebar heading

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.12' );
+define( 'DEWIT_THEME_VERSION', '0.2.13' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
