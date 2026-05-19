432b5618c02e26ad30cb18b0123b3eb29af8858e
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-19 12:25:55 +0200

    Fix mobile shop content flow

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.11' );
+define( 'DEWIT_THEME_VERSION', '0.2.12' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
