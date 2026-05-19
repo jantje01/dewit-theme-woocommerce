9d1f9e8f17d58486d749d741e832f8d6e2b5645d
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-19 12:00:20 +0200

    Align sidebar category arrows

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.7' );
+define( 'DEWIT_THEME_VERSION', '0.2.8' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
