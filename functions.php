9f5228c068ea4b6d335524cee565ba0010b8d6f2
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-19 11:12:34 +0200

    Refine sidebar and product grid container styling

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.0' );
+define( 'DEWIT_THEME_VERSION', '0.2.1' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
