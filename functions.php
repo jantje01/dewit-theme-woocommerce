1e0a17de1a7f35afcee2c8658ec8cb6882d7449f
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-19 11:53:57 +0200

    Refine shop grid spacing

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.5' );
+define( 'DEWIT_THEME_VERSION', '0.2.6' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
