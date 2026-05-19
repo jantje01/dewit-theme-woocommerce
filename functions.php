baa7f0b09d61eeb6e48ff9baad890de1ea98ef9e
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-19 12:03:28 +0200

    Move sidebar category arrows left

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.8' );
+define( 'DEWIT_THEME_VERSION', '0.2.9' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
