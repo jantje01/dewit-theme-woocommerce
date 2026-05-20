4be832fa58e511bac93645bb9d936726cb12fe5d
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-20 09:03:00 +0200

    Fix mobile shop toolbar overlap

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.16' );
+define( 'DEWIT_THEME_VERSION', '0.2.17' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
