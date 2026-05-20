cfedb4c0af4e93bee45eee3c8f2d83a85d589b85
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-20 10:50:17 +0200

    Add WooCommerce product detail page

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.20' );
+define( 'DEWIT_THEME_VERSION', '0.2.21' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
