05fc531dc0e9c8cd770a539b5800acc2d66fac85
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-20 09:40:19 +0200

    Tighten mobile toolbar offset

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.18' );
+define( 'DEWIT_THEME_VERSION', '0.2.19' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
