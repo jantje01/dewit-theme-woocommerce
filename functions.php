54a09adf20cff8b1dd8165d5a9115e4264ebd60e
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-20 09:48:43 +0200

    Disable mobile shop toolbar sticky

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.19' );
+define( 'DEWIT_THEME_VERSION', '0.2.20' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
