0b560c707f70ae964ca1d06393ad23c3edaa5f9b
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-21 13:50:08 +0200

    Fix mobile shop page ending

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.28' );
+define( 'DEWIT_THEME_VERSION', '0.2.29' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
