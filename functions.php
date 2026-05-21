7ceadf9c2e1d986a8be24cdd9c6f6a39a9670161
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-21 15:05:28 +0200

    Prevent sidebar category flash

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.33' );
+define( 'DEWIT_THEME_VERSION', '0.2.34' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
