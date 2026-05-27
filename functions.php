7db126975c747e36a5998e0b0b1ff683ed6f953e
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-27 16:19:51 +0200

    Style product grid scrollbar

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.67' );
+define( 'DEWIT_THEME_VERSION', '0.2.68' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
