5d509461f4cd2f85d43bc077e3ebd142a9e24107
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-27 15:41:38 +0200

    Fix shop archive overflow scroll

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.62' );
+define( 'DEWIT_THEME_VERSION', '0.2.63' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
