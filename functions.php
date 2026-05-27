4f6258e9acff03ef1007da5240416f2f67b17989
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-27 15:46:30 +0200

    Make product grid the scroll container

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.63' );
+define( 'DEWIT_THEME_VERSION', '0.2.64' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
