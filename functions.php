c320190043a9342818a894da7290fe8bbfa378d9
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-27 13:31:17 +0200

    Constrain grouped product card width

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.57' );
+define( 'DEWIT_THEME_VERSION', '0.2.58' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
