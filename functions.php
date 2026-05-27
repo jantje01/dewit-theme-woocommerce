050b95da74f22ea2107b68425b07c8e78576c4ea
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-27 13:47:20 +0200

    Use document scroll for shop archive

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.59' );
+define( 'DEWIT_THEME_VERSION', '0.2.60' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
