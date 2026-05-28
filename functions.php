0b7ec30264616d04e1a6c028818590187694e72e
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-28 13:42:58 +0200

    Refine grouped category headings

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.83' );
+define( 'DEWIT_THEME_VERSION', '0.2.84' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
