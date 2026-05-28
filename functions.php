7b762e96cff87d4bc13523096eb780f9cff6d5b4
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-28 13:54:51 +0200

    Nudge grouped heading text

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.84' );
+define( 'DEWIT_THEME_VERSION', '0.2.85' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
