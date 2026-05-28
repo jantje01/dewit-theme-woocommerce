17a90819f2bfabdf77cad996725e836a4efab615
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-28 11:17:31 +0200

    Test solid sidebar shape

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.76' );
+define( 'DEWIT_THEME_VERSION', '0.2.77' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
