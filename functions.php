562a8e3e6fe22402afa1a27c9e5c2f796ca68c60
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-28 09:19:01 +0200

    Let product images touch card edge

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.70' );
+define( 'DEWIT_THEME_VERSION', '0.2.71' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
