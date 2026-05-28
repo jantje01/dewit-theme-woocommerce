d5af75e2214a1bd333f963a846944ab6dea5af79
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-28 11:25:52 +0200

    Flush sidebar to viewport edge

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.78' );
+define( 'DEWIT_THEME_VERSION', '0.2.79' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
