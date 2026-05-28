82af4cec94bfcce2f39e674e2aa0aa0d74ef37fb
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-28 14:36:09 +0200

    Hide email toolbar button on mobile

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.88' );
+define( 'DEWIT_THEME_VERSION', '0.2.89' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
