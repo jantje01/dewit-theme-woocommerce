69784215631927f4e011b899887fcc716b0d5d79
Julius Jans <julius@dewitbouwmachines.nl>
2026-05-28 11:14:19 +0200

    Match product grid border radius

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.75' );
+define( 'DEWIT_THEME_VERSION', '0.2.76' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
