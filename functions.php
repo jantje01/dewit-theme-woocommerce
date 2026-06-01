41049364daf25a8592f17b50ab1225cb87c6859f
Julius Jans <julius@dewitbouwmachines.nl>
2026-06-01 10:48:40 +0200

    Reduce sidebar category text size

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.2.91' );
+define( 'DEWIT_THEME_VERSION', '0.2.92' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
 	/**
