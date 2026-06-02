371e13317bddc59391b504acbfc1d78046bbe220
Julius Jans <julius@dewitbouwmachines.nl>
2026-06-02 10:02:18 +0200

    Move grouped product scrolling inward

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.3.4' );
+define( 'DEWIT_THEME_VERSION', '0.3.5' );
 define( 'DEWIT_DEFAULT_PARENT_CATEGORY_SLUG', 'steigermateriaal' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
