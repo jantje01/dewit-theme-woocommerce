330347f1576a4a47a13074edb88e502f9957d11f
Julius Jans <julius@dewitbouwmachines.nl>
2026-06-02 10:34:09 +0200

    Use stable mobile viewport height for shop grid

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'DEWIT_THEME_VERSION', '0.3.8' );
+define( 'DEWIT_THEME_VERSION', '0.3.9' );
 define( 'DEWIT_DEFAULT_PARENT_CATEGORY_SLUG', 'steigermateriaal' );
 
 if ( ! function_exists( 'dewit_theme_setup' ) ) {
